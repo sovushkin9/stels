2dd146455f4684687584ff0c2020a03e7fd5ff3d
Lytvynenko Valerii <sovushkin9@gmail.com>
2018-05-15 11:22:10 +0300

    responsive for product card page

--- app/js/common.js
+++ app/js/common.js
@@ -3,8 +3,8 @@ $(function() {
 		infinite: true,
 		slidesToShow: 3,
 		slidesToScroll: 1,
-		nextArrow: $('.product-card__slider-prev'),
-        prevArrow: $('.product-card__slider-next'),
+		nextArrow: $('.product-card__slider').parent().find('.product-card__slider-prev'),
+        prevArrow: $('.product-card__slider').parent().find('.product-card__slider-next'),
         responsive: [
 	    {
 	      breakpoint: 760,
@@ -33,6 +33,44 @@ $(function() {
 	  ]
 	});
 
+
+	$('.product-cake__inner_similar').slick({
+		infinite: true,
+		slidesToShow: 1,
+		slidesToScroll: 1,
+		mobileFirst: true,
+		centerMode: true,
+		centerPadding: 30,
+		nextArrow: $('.product-cake__inner_similar').parent().find('.product-card__slider-prev'),
+        prevArrow: $('.product-cake__inner_similar').parent().find('.product-card__slider-next'),
+		responsive: [
+            {
+                breakpoint: 600,
+                settings: "unslick"
+            }
+        ]	
+	});
+
+	$('.product-cake__inner_property').slick({
+		infinite: true,
+		slidesToShow: 1,
+		slidesToScroll: 1,
+		mobileFirst: true,
+		nextArrow: $('.product-cake__inner_property').parent().find('.product-card__slider-prev'),
+        prevArrow: $('.product-cake__inner_property').parent().find('.product-card__slider-next'),
+		responsive: [
+            {
+                breakpoint: 600,
+                settings: "unslick"
+            }
+        ]	
+	});
+
+	$(window).on('resize orientationchange', function() {
+  		$('.product-cake__inner_similar').slick('resize');
+  		$('.product-cake__inner_property').slick('resize');
+	});
+
 	function geometryTab(){
 		$('.geometry-tab span').click(function(){
 			var btns= $(this).parent().find('span');
@@ -55,8 +93,10 @@ $(function() {
 
 
 	function sizing () {
-		var marginLeft= $('.page-container').css('margin-left');
-		$('.category-banner__desc').css('margin-left',marginLeft);
+		if($(window).width() > 899){
+			var marginLeft= $('.page-container').css('margin-left');
+			$('.category-banner__desc').css('margin-left',marginLeft);
+		} else {$('.category-banner__desc').css('margin-left',0);}
 	}
 	sizing();
 	$(window).resize(sizing);
