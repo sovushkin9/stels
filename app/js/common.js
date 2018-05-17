bf1fbc116551f2f3bef675f20efecb33cbe4115e
Lytvynenko Valerii <sovushkin9@gmail.com>
2018-05-17 12:37:08 +0300

    scroll for menu on product page

--- app/js/common.js
+++ app/js/common.js
@@ -209,32 +209,38 @@ $(function() {
 			if($(window).width()<600){
 				$(this).find('.product-card__tab-picker').toggleClass('active');
 				$(this).siblings('.product-card__tab-body').slideToggle();
+				$('.compare__product').slick();
+  				$('.compare__slider-inner').slick();
 			}
 		})
 	}
 
 	partHeadingTab();
 
+
 	function createSticky(sticky) {
 	
 	if (sticky.length!==0) {
 
-	var	pos = sticky.offset().top;
-	$(window).on("scroll", function() {
-    		if ($(window).scrollTop() >= pos){
-     			$('body').css('padding-top',sticky.height());
-     			sticky.addClass("fixed");
-     		}
-     		else {
-     			$('body').css('padding-top',0);
-     			sticky.removeClass("fixed");
-     		}      
- 		});			
-	 	}
+		var	pos = sticky.offset().top;
+		
+		$(window).on("scroll", function() {
+	    		if ($(window).scrollTop() >= pos){
+	     			$('body').css('padding-top',sticky.height());
+	     			sticky.addClass("fixed");
+	     		}
+	     		else {
+	     			$('body').css('padding-top',0);
+	     			sticky.removeClass("fixed");
+	     		}      
+	 		});			
+		 }
 	 }
 
+	createSticky($(".product-card__nav"));
 	createSticky($(".compare__table-nav"));
 	
+
 	function productListSlider() {
 		var slider = $('.product__slider-big')
 		slider.on('init', function() {
@@ -280,5 +286,20 @@ $(function() {
 		})
 	}
 	productListSlider()
+
+
+	function scrollNav(){
+		$('a[href^="#"]').on('click', function(event) {
+	    var target = $(this.getAttribute('href'));
+	    if( target.length ) {
+	        event.preventDefault();
+	        $('html, body').stop().animate({
+	            scrollTop: target.offset().top
+	        }, 700);
+	    }
+		});
+	}
+
+	scrollNav();
 
 });
